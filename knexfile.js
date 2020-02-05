190ea0fdf183b023167aa8cbc168f19dbd6b5564
talgat065 <talgat06585@gmail.com>
2020-02-05 20:35:23 +0600

    cleanup

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host:     '127.0.0.1',
-      database: 'metrika',
-      user:     'root',
-      password: ''
+      host:     process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     migrations: {
       tableName: 'migrations'
